3e460accec562809932e6bbcca4347c3743b3b41
MiK <therealilyaskhan@gmail.com>
2024-09-14 15:16:19 +0500

    facebook pixel added

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
+import Script from "next/script";
 import OrderDetails from "../components/OrderDetails";
 import ErrorComponent from "../components/ErrorComponent";
+import { useEffect } from "react";
 
 const localeTitles = {
   en: "Order Successful",
@@ -10,45 +12,60 @@ const localeDescriptions = {
   en: "Thank you for your order",
   de: "Vielen Dank für Ihre Bestellung",
 };
+const localizedLabels = {
+  en: {
+    thankYouLabel: "Thank You",
+    orderConfirmedText: "Your order has been confirmed",
+    orderConfirmedSubText:
+      "You will receive an email once your order is ready.",
+    orderDetailsLabel: "Order Details",
+    contactInfoLabel: "Contact Information",
+    shippingAddressLabel: "Shipping Address",
+    paymentDetailsLabel: "Payment Details",
+    shippingMethodLabel: "Shipping Method",
+    shippingChargesLabel: "FREE",
+  },
+  de: {
+    thankYouLabel: "Danke",
+    orderConfirmedText: "Ihre Bestellung ist bestätigt",
+    orderConfirmedSubText:
+      "Sie erhalten eine E-Mail, sobald Ihre Bestellung fertig ist.",
+    orderDetailsLabel: "Bestelldetails",
+    contactInfoLabel: "Kontaktinformationen",
+    shippingAddressLabel: "Lieferadresse",
+    paymentDetailsLabel: "Zahlungsdetails",
+    shippingMethodLabel: "Versandart",
+    shippingChargesLabel: "KOSTENLOS",
+  },
+};
+
+const getLocalizedComponent = (locale, sessionData) => (
+  <OrderDetails sessionData={sessionData} locale={localizedLabels[locale]} />
+);
+
+const getTitle = (locale) => localeTitles[locale] || localeTitles.de;
+const getDescription = (locale) =>
+  localeDescriptions[locale] || localeDescriptions.de;
 
 const Home = ({ sessionData, locale, hasError }) => {
-  const getLocalizedComponent = (locale) => {
-    const en = {
-      thankYouLabel: "Thank You",
-      orderConfirmedText: "Your order has been confirmed",
-      orderConfirmedSubText:
-        "You will receive an email once your order is ready.",
-      orderDetailsLabel: "Order Details",
-      contactInfoLabel: "Contact Information",
-      shippingAddressLabel: "Shipping Address",
-      paymentDetailsLabel: "Payment Details",
-      shippingMethodLabel: "Shipping Method",
-      shippingChargesLabel: "FREE",
-    };
-    const de = {
-      thankYouLabel: "Danke",
-      orderConfirmedText: "Ihre Bestellung ist bestätigt",
-      orderConfirmedSubText:
-        "Sie erhalten eine E-Mail, sobald Ihre Bestellung fertig ist.",
-      orderDetailsLabel: "Bestelldetails",
-      contactInfoLabel: "Kontaktinformationen",
-      shippingAddressLabel: "Lieferadresse",
-      paymentDetailsLabel: "Zahlungsdetails",
-      shippingMethodLabel: "Versandart",
-      shippingChargesLabel: "KOSTENLOS",
-    };
-    switch (locale) {
-      case "en":
-        return <OrderDetails sessionData={sessionData} locale={en} />;
-      case "de":
-      default:
-        return <OrderDetails sessionData={sessionData} locale={de} />;
+  // Facebook Pixel Purchase Event Trigger
+  useEffect(() => {
+    if (
+      !hasError &&
+      sessionData &&
+      sessionData.amount_subtotal &&
+      sessionData.currency
+    ) {
+      // Trigger the purchase event
+      window.fbq("track", "Purchase", {
+        value: (sessionData.amount_subtotal / 100).toFixed(2),
+        currency: sessionData.currency.toUpperCase(),
+      });
     }
-  };
+  }, [sessionData, hasError]);
 
-  const title = localeTitles[locale] || "Bestellung erfolgreich";
-  const description =
-    localeDescriptions[locale] || "Vielen Dank für Ihre Bestellung";
+  const title = getTitle(locale);
+  const description = getDescription(locale);
 
   return (
     <>
@@ -57,22 +74,46 @@ const Home = ({ sessionData, locale, hasError }) => {
         <meta name='description' content={description} />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
+        {/* FACEBOOK PIXEL BASE CODE */}
+        <Script id='facebook-pixel' strategy='beforeInteractive'>
+          {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '536006748934294');
+        `}
+        </Script>
+        <noscript>
+          <img
+            height='1'
+            width='1'
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=536006748934294&ev=PageView&noscript=1`}
+          />
+        </noscript>
       </Head>
       {hasError ? (
         <ErrorComponent
           noInfo={locale === "de" ? "Nichts gefunden!" : "Nothing found!"}
         />
       ) : (
-        getLocalizedComponent(locale)
+        getLocalizedComponent(locale, sessionData)
       )}
     </>
   );
 };
 
 export async function getServerSideProps({ query }) {
-  const sessionId = query.session_id;
-  const stripeId = query.stripe_account_id;
-  const locale = query.lang || "de";
+  const {
+    session_id: sessionId,
+    stripe_account_id: stripeId,
+    lang: locale = "de",
+  } = query;
 
   if (!sessionId || !stripeId) {
     return {
